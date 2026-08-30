57277b94210f69181610ae6d8abde07eb99524ae
Aditya Negi <01adityanegi@gmail.com>
2026-08-30 21:31:40 +0530

    Implement rounds and target score in dice rolling
    Add functionality for multiple rounds and target score.

--- Dice_Roll.py
+++ Dice_Roll.py
@@ -1,3 +1,11 @@
 import random 
 def roll_dice(side = 6):
       return random.randint (1, sides)
+rounds = int (input("How many round ? "))
+target = int(input("Target score to reach: "))
+total = 0 
+history = []
+round_num = 0
+while total < target and round_num < rounds:
+      roll = roll_dice
+      total += roll
