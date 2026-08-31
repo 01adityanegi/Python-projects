32b4f8784002e3e022b6ec2d1c3367ed04a1323b
Aditya Negi <01adityanegi@gmail.com>
2026-08-31 22:28:32 +0530

    Add input prompts for mad libs story generation
    Added prompts for noun, verb, adjective, and place inputs.

--- mad_libs_generator.py
+++ mad_libs_generator.py
@@ -1,2 +1,6 @@
 def mad_libs():
     print("\n Let's creates a story! Fill in the blanks.")
+    noun = input("Noun : ")
+    verb = input("Verb : ")
+    adjective = input("Adjective : ")
+    place = input("place : ")
