77db743d64349fc81d43884582cf06364a65de2a
Aditya Negi <01adityanegi@gmail.com>
2026-09-01 23:36:32 +0530

    Add trivia questions to trivia_quiz.py

---
+++ trivia_quiz.py
@@ -0,0 +1,7 @@
+question = {
+    "what is the captial of India?" :"New Dehli",
+    "Which planet is know as the red planet? " : "Mars",
+    "what language is this ?": "Python",
+    "Who created the Python programming language?": "Guido van Rossum",
+    " Which keyword is used to define a function in Python?": "def"
+}
