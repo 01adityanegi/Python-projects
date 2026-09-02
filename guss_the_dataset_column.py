809375343842a7ea223d402929e3a55816a44845
Aditya Negi <01adityanegi@gmail.com>
2026-09-02 23:37:25 +0530

    Fix guessing logic and improve user feedback

--- guss_the_dataset_column.py
+++ guss_the_dataset_column.py
@@ -7,3 +7,10 @@ while attempts >0:
     display = " ".join(c if c in guessed else "_" for c in word)
     print(f"Word: {display} Attempts left: {attempts}")
     guess = input("Guess a letter : ").lower()
+     if guess in guessed:
+        print("You already guessed that letter . ")
+        continue
+    guessed.add(guess)
+    if guess not in word : 
+        attempts -=1
+        print("Not in the word.")
