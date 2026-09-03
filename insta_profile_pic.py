62b0dfe83fbc640845e8d780201c37bc96ce0c11
Aditya Negi <01adityanegi@gmail.com>
2026-09-03 21:52:04 +0530

    Enhance clarity with comments and formatting
    Added comments to clarify the purpose of the targets list and improved output formatting.

--- insta_profile_pic.py
+++ insta_profile_pic.py
@@ -2,4 +2,8 @@ import instaloader
 import requests
 
 ig = instaloader.Instaloader()
-targets = ["adityanegi01" , "sujal_uk01_", "filmmakerpriyanshu","kunalrana___"]
+targets = ["adityanegi01" , "sujal_uk01_", "filmmakerpriyanshu","kunalrana___"] //instragram username 
+
+for username in targets:
+    print(f"\n{'='*40}")
+    print(f"Scanning :{username}")
