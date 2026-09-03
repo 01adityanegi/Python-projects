95e3518dd376ca666681ecbc15f93d215624e3ca
Aditya Negi <01adityanegi@gmail.com>
2026-09-03 21:57:49 +0530

    Implement profile picture download feature
    Added functionality to save Instagram profile picture.

--- insta_profile_pic.py
+++ insta_profile_pic.py
@@ -10,4 +10,16 @@ for username in targets:
 
     profile = instaloader.Profile.from_username(ig.context, username)
 
-    
+     print(f"Name : {profile.full_name}")
+    print(f"Followers : {profile.followers}")
+    print(f"Following : {profile.followees}")
+    print(f"Bio : {profile.biography}")
+
+    dp_url= profile.profile_pic_url
+    response = requests.get(dp_url)
+
+    with open(f"{username}_dp.jpg","wb") as file:
+        file.write(response.content)
+
+    print(f"Saved As : {username}_dp.jpg")
+    print(f"{'=*40'}")
